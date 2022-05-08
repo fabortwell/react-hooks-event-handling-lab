cb51722d61456b3bbe7ccbfc01f1defbe105765e
Brian Sitati <briansit93@gmail.com>
2022-05-08 11:32:29 +0300

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+function EyesOnMe() {
+    function focuseventHandler() {
+        console.log("Good")
+    }
+    function blurEventHandler() {
+        console.log("Hey! Eyes on me")
+    }
+    return <button onFocus={focuseventHandler} onBlur={blurEventHandler}>Eyes on me</button>
+} 
+export default EyesOnMe
